fab78937c5ed71c9a1025bd76e72d567282059c2
YANN HAMDI <hamdiyann@gmail.com>
2021-03-20 21:20:15 +0100

    travail sur base de donnees

--- projet13/movies/import_api.py
+++ projet13/movies/import_api.py
@@ -9,13 +9,17 @@ api_movie_db = os.environ.get("API_MOVIE_DB")
 
 def get_json():
     """method that look for best rated movies api"""
-    response = requests.get("https://api.themoviedb.org/3/movie/top_rated?api_key="+ api_movie_db + "&language=en-US&page=1")
-
-
-
+    response = requests.get("https://api.themoviedb.org/3/movie/top_rated?api_key="+ api_movie_db + "&language=en-US")
     movies = []
-    movies = response.json()["results"][19]["release_date"]
-    print(movies)
+    movies = response.json()
+    i = 0
+    movies_title = []
+    movies_release_date = []
+    for i in range(len(movies["results"])):
+        movies_title.append((movies["results"][i]["title"]))
+        movies_release_date.append((movies["results"][i]["release_date"]))
+    print(movies_title)
+    print(movies_release_date)
     url = "https://movie-database-imdb-alternative.p.rapidapi.com/"
 
     querystring = {"i":"tt0109830","r":"json"}
@@ -29,18 +33,17 @@ def get_json():
     movies_details = []
     movies_details = response.json()
     url_2 = "https://movie-database-imdb-alternative.p.rapidapi.com/"
-    querystring_2 = {"s":"Forrest Gump","page":"1","y":"1994-07-06","r":"json"}
+    querystring_2 = {"s": "forrest gump","page":"1","r":"json"}
 
     headers_2 = {
     'x-rapidapi-key': api_key,
     'x-rapidapi-host': "movie-database-imdb-alternative.p.rapidapi.com"
     }
-
     response_2 = requests.request("GET", url, headers=headers_2, params=querystring_2)
     response_details = []
-    response_details = response_2.json()
-    print(response_details)
-
+    if response_2.status_code == 200:
+        response_details = response_2.json()["Search"][0]["imdbID"]
+        print(response_details)
     
     
     
