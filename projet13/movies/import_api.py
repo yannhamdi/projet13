f4a91097c00e380db1d74dc463463e548cd46a1f
YANN HAMDI <hamdiyann@gmail.com>
2021-03-21 23:55:42 +0100

    données

--- projet13/movies/import_api.py
+++ projet13/movies/import_api.py
@@ -20,35 +20,27 @@ def get_json():
     'x-rapidapi-host': "imdb8.p.rapidapi.com"
     }
     num_page = 1
-    for num_page in range(1, 6):
+    movies_title = []
+    movies_id = []
+    for num_page in range(1,6):
         response = requests.get("https://api.themoviedb.org/3/movie/top_rated?api_key=" + api_movie_db + "&language=en-US&page=" + str(num_page))
         movies = []
         movies = response.json()
-        i = 0
-        movies_title = []
-    for i in range(len(movies["results"])):
+        for i in range(len(movies["results"])):
             movies_title.append((movies["results"][i]["title"]))
-            response_details = []
-    j = 0
-    for j in range(len(movies_title)):
+    for j in range(0, len(movies_title)):
         querystring = {"q": movies_title[j]}
         response_2 = requests.request("GET", url_2, headers=headers_2, params=querystring)
         try:
-            response_details.append(response_2.json()["d"][0]["id"])
+            movies_id.append(response_2.json()["d"][0]["id"])
+            print(len(movies_id))
         except:
             continue
-    print(response_details)
-    h = 0
-    movies_details = []
-    for h in range(len((response_details))-1):
-        querystring_2 = {"i": response_details[h],"r":"json"}
-        response_3 = requests.request("GET", url, headers=headers, params=querystring_2) 
-        try:
-            movies_details.append(response_3.json())
-        except:
-            continue
-    print(len(movies_details))
-            
+
+    print(len(movies_id))
+        
+
+    
     
 
 
