068e040162485c196b0475e3242f83df8a32465d
YANN HAMDI <hamdiyann@gmail.com>
2021-03-21 11:18:14 +0100

    extraction données api

--- projet13/movies/import_api.py
+++ projet13/movies/import_api.py
@@ -9,46 +9,40 @@ api_movie_db = os.environ.get("API_MOVIE_DB")
 
 def get_json():
     """method that look for best rated movies api"""
-    response = requests.get("https://api.themoviedb.org/3/movie/top_rated?api_key="+ api_movie_db + "&language=en-US")
-    movies = []
-    movies = response.json()
-    i = 0
-    movies_title = []
-    movies_release_date = []
-    for i in range(len(movies["results"])):
-        movies_title.append((movies["results"][i]["title"]))
-        movies_release_date.append((movies["results"][i]["release_date"]))
-    print(movies_title)
-    print(movies_release_date)
-    
-
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    movies_details = []
-    movies_details = response.json()
-    url_2 = "https://movie-database-imdb-alternative.p.rapidapi.com/"
-    querystring_2 = {"s": "forrest gump","page":"1","r":"json"}
-
-    headers_2 = {
-    'x-rapidapi-key': api_key,
-    'x-rapidapi-host': "movie-database-imdb-alternative.p.rapidapi.com"
-    }
-    response_2 = requests.request("GET", url, headers=headers_2, params=querystring_2)
-    response_details = []
-    if response_2.status_code == 200:
-        response_details = response_2.json()["Search"][0]["imdbID"]
-        print(response_details)
     url = "https://movie-database-imdb-alternative.p.rapidapi.com/"
-
-    querystring = {"i":"tt0109830","r":"json"}
-
     headers = {
     'x-rapidapi-key': api_key,
     'x-rapidapi-host': "movie-database-imdb-alternative.p.rapidapi.com"
     }
-    
-    
-    
+    url_2 = "https://imdb8.p.rapidapi.com/auto-complete"
+    headers_2 = {
+    'x-rapidapi-key': api_key,
+    'x-rapidapi-host': "imdb8.p.rapidapi.com"
+    }
 
+    response = requests.get("https://api.themoviedb.org/3/movie/top_rated?api_key="+ api_movie_db + "&language=en-US&page=1")
+    movies = []
+    movies = response.json()
+    i = 0
+    movies_title = []
+    for i in range(len(movies["results"])):
+        movies_title.append((movies["results"][i]["title"]))
+    response_details = []
+    
+    j = 0
+    for j in range(len(movies_title)):
+        querystring = {"q": movies_title[j]}
+        response_2 = requests.request("GET", url_2, headers=headers_2, params=querystring)
+        if response_2.status_code == 200:
+            response_details.append(response_2.json()["d"][0]["id"])
+    h = 0
+    movies_details = []
+    for h in range(len(response_details)):
+        querystring_2 = {"i": response_details[h],"r":"json"}
+        response_3 = requests.request("GET", url, headers=headers, params=querystring_2) 
+        if response_3.status_code == 200:
+            movies_details.append(response_3.json())
+            
     
     
 
