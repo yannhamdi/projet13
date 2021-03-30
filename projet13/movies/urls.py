12f9bb5f1af5fc2498ee392c5cc52fbfbfb61ac9
YANN HAMDI <hamdiyann@gmail.com>
2021-03-30 21:58:17 +0100

    utilisation session pour pagination

--- projet13/movies/urls.py
+++ projet13/movies/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('movie/<int:id>', views.lire, name='lire'),
     path('home', views.home, name='home'),
     path('search', views.search, name='search'),
+ 
 ]
--- projet13/movies/views.py
+++ projet13/movies/views.py
@@ -24,33 +24,26 @@ def home(request):
 
 
 def search(request):
-    form = ProductSearch(request.POST or None)
-    if form.is_valid():
-        prod = form.cleaned_data['search']
-        search_sub = Movie.objects.search_movie_title(prod)
-        if search_sub:
-            paginator = Paginator(search_sub, 10)
+    if not request.method == 'POST':
+        if 'search' in request.session:
+            request.POST = request.session['search']
+            request.method = 'POST'
+            
+    if request.method == 'POST':
+        form = ProductSearch(request.POST or None)
+        request.session['search']  = request.POST
+        if form.is_valid():
+            prod = form.cleaned_data['search']
+            list_movie_cat = Category.objects.search_movie_genre(prod)
+            list_movie_by_cat = Movie.objects.filter(category__in=list_movie_cat)
+            paginator_1 = Paginator(list_movie_by_cat, 10)
             page_number = request.GET.get('page')
-            page_obj = paginator.get_page(page_number)
-            return render(request, 'movies/index.html', {'page_obj': page_obj})
-        
-        list_movie_cat = Category.objects.search_movie_genre(prod)
-        if list_movie_cat:
-            list_movie_by_cat = Movie.objects.filter(category__in=list_movie_cat)	
-            paginator = Paginator(list_movie_by_cat, 10)
-            page_number = request.GET.get('page')
-            page_obj = paginator.get_page(page_number)
-            return render(request, 'movies/index.html', {'page_obj': page_obj})
-        list_movie_actor  = Actor.objects.search_movie_actor(prod)      
-        if list_movie_actor:
-            list_movie_by_actor  = Movie.objects.filter(actor__in=list_movie_actor)
+            page_obj = paginator_1.get_page(page_number)
 
-            paginator = Paginator(list_movie_by_actor, 10)
-            page_number = request.GET.get('page')
-            page_obj = paginator.get_page(page_number)
-            return render(request, 'movies/index.html', {'page_obj': page_obj})
-        return render(request, 'movies/no_response.html')
-    return render(request, 'movies/search.html', locals())
+    else:
 
-
+        form = ProductSearch(request.POST or None)
+        return render(request, 'movies/search.html', {'form':form})
+    return render(request, 'movies/index.html', {'page_obj': page_obj})
+    
     
