75f02c0e9bea02f87becfe08d36afdb1879c42d9
YANN HAMDI <hamdiyann@gmail.com>
2021-03-30 22:36:26 +0100

    test session

--- projet13/movies/views.py
+++ projet13/movies/views.py
@@ -26,20 +26,24 @@ def home(request):
 def search(request):
     if not request.method == 'POST':
         if 'search' in request.session:
+            print('search get')
             request.POST = request.session['search']
             request.method = 'POST'
-            
+        form = ProductSearch()  
     if request.method == 'POST':
+        print('post')
         form = ProductSearch(request.POST or None)
         request.session['search']  = request.POST
         if form.is_valid():
+            print('valid')
             prod = form.cleaned_data['search']
             list_movie_cat = Category.objects.search_movie_genre(prod)
             list_movie_by_cat = Movie.objects.filter(category__in=list_movie_cat)
             paginator_1 = Paginator(list_movie_by_cat, 10)
             page_number = request.GET.get('page')
             page_obj = paginator_1.get_page(page_number)
-        
+    else:
+        print('else')
         form = ProductSearch()
         return render(request, 'movies/search.html', {'form': form})
     return render(request, 'movies/index.html', {'page_obj': page_obj})
