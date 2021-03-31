22cc1e4b780fa87c5dfb88689e647bb659074bed
YANN HAMDI <hamdiyann@gmail.com>
2021-03-31 10:41:02 +0100

    vue recherche fonctionnelle

--- projet13/movies/views.py
+++ projet13/movies/views.py
@@ -33,13 +33,30 @@ def search(request):
         form = ProductSearch(request.POST or None)
         request.session['search']  = request.POST
         if form.is_valid():
-            print('valid')
             prod = form.cleaned_data['search']
             list_movie_cat = Category.objects.search_movie_genre(prod)
-            list_movie_by_cat = Movie.objects.filter(category__in=list_movie_cat)
-            paginator_1 = Paginator(list_movie_by_cat, 10)
-            page_number = request.GET.get('page')
-            page_obj = paginator_1.get_page(page_number)
+            search_sub = Movie.objects.search_movie_title(prod)
+            list_movie_by_actor  = Actor.objects.search_movie_actor(prod) 
+            list_movie_cat = Category.objects.search_movie_genre(prod)
+            if list_movie_by_actor:
+                list_movie_actor  = Movie.objects.filter(actor__in=list_movie_by_actor)
+                paginator_1 = Paginator(list_movie_actor, 10)
+                page_number = request.GET.get('page')
+                page_obj = paginator_1.get_page(page_number)
+                return render(request, 'movies/index.html', {'page_obj': page_obj})
+            if list_movie_cat:
+                list_movie_by_cat = Movie.objects.filter(category__in=list_movie_cat)	
+                paginator = Paginator(list_movie_by_cat, 10)
+                page_number = request.GET.get('page')
+                page_obj = paginator.get_page(page_number)
+                return render(request, 'movies/index.html', {'page_obj': page_obj})
+            if search_sub:
+                paginator = Paginator(search_sub, 10)
+                page_number = request.GET.get('page')
+                page_obj = paginator.get_page(page_number)
+                return render(request, 'movies/index.html', {'page_obj': page_obj})
+            return render(request, 'movies/no_response.html')
+
     else:
         form = ProductSearch()
         return render(request, 'movies/search.html', {'form': form})
