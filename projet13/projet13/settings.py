8dd17f1be757c70835edfbbeb9b8288f3df070fa
YANN HAMDI <hamdiyann@gmail.com>
2021-04-01 21:18:15 +0100

    reset db

--- projet13/projet13/settings.py
+++ projet13/projet13/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django_extensions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
