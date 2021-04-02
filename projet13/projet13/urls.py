60f39d729bfe30997a90ea7d1f5b2ffbf7c37950
YANN HAMDI <hamdiyann@gmail.com>
2021-04-02 12:38:13 +0100

    application favorites + tests

--- projet13/projet13/urls.py
+++ projet13/projet13/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('movies/', include('movies.urls')),
     path('users/', include('users.urls')),
+    path('favorites/', include('favorites.urls')),
    
 ]
