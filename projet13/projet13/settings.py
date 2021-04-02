84000688bb3726fb2c8d5866a88a7d33b7a14bd6
YANN HAMDI <hamdiyann@gmail.com>
2021-04-02 20:57:33 +0100

    preparation frontend

--- projet13/projet13/settings.py
+++ projet13/projet13/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'projet13.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR), 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -135,3 +135,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static")),
--- projet13/projet13/urls.py
+++ projet13/projet13/urls.py
@@ -1,10 +1,12 @@
 from django.urls import include, path
 from django.contrib import admin
+from movies.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('movies/', include('movies.urls')),
     path('users/', include('users.urls')),
     path('favorites/', include('favorites.urls')),
+     path('', home, name="home"),
    
 ]
