f88ba99880adcbab7a8ff2db1a05d367422ec17a
YANN HAMDI <hamdiyann@gmail.com>
2021-04-04 15:25:08 +0100

    resultats

--- projet13/favorites/views.py
+++ projet13/favorites/views.py
@@ -21,7 +21,7 @@ def display_account(request):
     form = ProductSearch(request.POST or None)
     favoris = Favorite.objects.filter(
         user_link=request.user).order_by('movie_saved')
-    paginator = Paginator(favoris, 3)
+    paginator = Paginator(favoris, 10)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     return render(request, 'display_account.html', {'page_obj': page_obj, 'form': form})
