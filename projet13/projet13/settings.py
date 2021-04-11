48bb2804ded22e7ae6644bdd96e6da135b5c6e55
YANN HAMDI <hamdiyann@gmail.com>
2021-04-11 13:26:22 +0100

    procfile

--- projet13/projet13/settings.py
+++ projet13/projet13/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 import dj_database_url
