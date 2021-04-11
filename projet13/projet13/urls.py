baecb5fc3b3155b0482932b6533052b643f1848f
YANN HAMDI <hamdiyann@gmail.com>
2021-04-11 13:16:43 +0100

    procfile

--- projet13/projet13/urls.py
+++ projet13/projet13/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 from django.contrib import admin
-from movies.views import home
+from projet13.movies.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
